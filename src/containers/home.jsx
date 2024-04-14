62bbb994e4853fb44753f3e777e1a968b38ec913
Dobro2910 <90589894+Dobro2910@users.noreply.github.com>
2024-04-14 20:23:36 +1000

    update code

---
+++ src/containers/home.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import './style.scss';
+
+const Home=()=>{
+    return (
+        <section id="home" className="home">
+            <div className="home_text-wrapper">
+                <h5>
+                    WELCOME TO
+                </h5>
+                <br/>
+                <h1>
+                    The Wellhall
+                </h1>
+                <br/>
+                <h3>
+                    Resort & Spa Hotel
+                </h3>
+            </div>
+        </section>
+    )
+}
+
+export default Home;
