2629e6bbad2618e0c863618b3d5c70bb9ecfbf1c
Dobro2910 <90589894+Dobro2910@users.noreply.github.com>
2024-04-21 01:59:24 +1000

    add ts particles

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import './App.scss';
 import {Routes, Route, useLocation} from 'react-router-dom'
 import Home from './containers/home';
+import ParticleEffect from "./components/particle/particleEffect";
 
 
 function App() {
 
   return (
     <div className="App">
-      {/* navbar
-      <Navbar /> */}
+      <ParticleEffect id="particles"/>
 
       {/* main page content */}
       <div className='App_main-page-content'>
